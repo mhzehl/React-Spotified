1467c3c4c633167b714a8a01a0a0c4cbc93c88ed
Melvin Zehl <mhzehl@gmail.com>
2017-02-11 12:38:31 +0100

    Add SongItem component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import searchSpotify from '../utils/searchSpotify';
+import SongItem from './SongItem/SongItem';
 
 export default class App extends Component {
 
@@ -8,18 +9,21 @@ export default class App extends Component {
     super(props);
     this.state = {
       song: '',
+      tracks: {},
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then((tracks) => console.log('tracks', tracks));
+      .then(({ tracks }) => this.setState({ tracks }));
   }
 
   render() {
+    const { tracks } = this.state;
     return (
       <div>
         <Searchbar updateText={(song) => this.setState({ song })} fetchSongs={this.fetchSongs} />
+        {tracks.items && <SongItem songData={tracks.items[0]} />}
       </div>
     );
   }
--- src/utils/searchSpotify.js
+++ src/utils/searchSpotify.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const searchSpotify = (searchItem) => new Promise((resolve, reject) => {
-  const result = axios.get(`https://api.spotify.com/version1/search?q=${searchItem}&type=track`);
+  const result = axios.get(`https://api.spotify.com/v1/search?q=${searchItem}&type=track`);
   result
     .then(({ data }) => resolve(data))
     .catch((err) => reject(err));
